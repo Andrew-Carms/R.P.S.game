cf1aba0b4fc38f11e133c9da144faa9ef49b0bd3
Andrew-Carms <31894659+Andrew-Carms@users.noreply.github.com>
2018-05-08 11:26:56 -0400

    updated main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from random import randint
+ 
+
+#Create a list of play options
+
+t = ["Rock", "Paper", "Scissors"]
+ 
+#Assign a random play to the computer
+
+AI = t[randint(0,2)]
+ 
+#Set player to False
+
+player = False
+ 
+while player == False:
