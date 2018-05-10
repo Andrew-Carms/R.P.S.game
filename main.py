073d07ff4b90d7bdfe215d3576219f07274a87d0
Andrew-Carms <31894659+Andrew-Carms@users.noreply.github.com>
2018-05-10 10:53:58 -0400

    updated main.py
    Working function for 'Player vs. AI' (I think)

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
 from random import randint
  
-
-#Create a list of play options
+#create a list of play options
 
 t = ["Rock", "Paper", "Scissors"]
  
-#Assign a random play to the computer
+#assign a random play to the computer
 
-AI = t[randint(0,2)]
+computer = t[randint(0,2)]
  
-#Set player to False
+#set player to False
 
 player = False
  
 while player == False:
+
+#set player to True
+    
+    player = input("Rock, Paper, Scissors.. SHOOT!")
+    
+    
+    if player == computer:
+        print("Tie!")
+    
+    
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "beats", computer)
+    
+    
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    
+    
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "beats", player)
+        else:
+            print("You win!", player, "cut", computer)
+    
+    
+    else:
+        print("Not a valid play, Try Again")
+    
+    
+    #player was set to True, want it to be False so the loop continues
+    
+    
+    player = False
+    computer = t[randint(0,2)]
+
